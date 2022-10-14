950aa7ef66cf294b7bc1bb007916c290bb945597
Eduard <eduard.cojocaru26@gmail.com>
2022-10-14 17:26:53 +0300

    Add create tasks functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-console.log('hello')
-axios
-  .get("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+const todoList = document.querySelector('.todolist');
+const taskList = todoList.querySelector('.todolist__tasks');
+
+todoList.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const newTaskField = todoList.querySelector('input');
+  const inputValue = newTaskField.value.trim();
+  console.log(inputValue);
+
+  newTaskField.value = "";
+  newTaskField.focus();
+  if (!inputValue) return;
+
+  const taskElement = createTaskElement(inputValue);
+
+  taskList.appendChild(taskElement);
+})
+
+const generateUniqueString = (length) =>
+  Math.random()
+    .toString(36)
+    .substring(2, 2 + length);
+
+const createTaskElement = (taskname) => {
+  const uniqueID = generateUniqueString(10);
+  const taskElement = document.createElement("li");
+  taskElement.classList.add("task");
+  taskElement.innerHTML = DOMPurify.sanitize(`
+  <input type="checkbox" id="${uniqueID}" />
+  <label for="${uniqueID}">
+    <svg viewBox="0 0 20 15">
+      <path d="M0 8l2-2 5 5L18 0l2 2L7 15z" fill-rule="nonzero" />
+    </svg>
+  </label>
+  <span class="task__name">${taskname}</span>
+  <button type="button" class="task__delete-button">
+    <svg viewBox="0 0 20 20">
+      <path
+        d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+      />
+    </svg>
+  </button>`);
+  return taskElement;
+};    
